bc7396e69e2da320b468ca5623d1567bea38a219
Rasul <kubatev87@gmail.com>
2020-07-13 17:06:59 +0400

    NewInit

--- js/script.js
+++ js/script.js
@@ -2,46 +2,66 @@ window.addEventListener('DOMContentLoaded', function()
 {
   'use strict';
 
-   let tab = document.querySelectorAll('.info-header-tab'),
-       info = document.querySelector('.info-header'),
-       tabContent = document.querySelectorAll('.info-tabcontent');
+   //SLIDER
+
+ let slideIndex = 1,
+ slides = document.querySelectorAll('.slider-item'),
+ prev = document.querySelector('.prev'),
+ next = document.querySelector('.next'),
+ dotsWrap = document.querySelector('.slider-dots'),
+ dots = document.querySelectorAll('.dot');
+
+ 
+ showSlides(slideIndex);
+function showSlides(n) {
+   
+
+   if(n > slides.length) {
+       slideIndex = 1;
+   }
+   if(n < 1) {
+       slideIndex = slides.length;
+   }
+
+     slides.forEach((item) => item.style.display = 'none'); 
+    //    for(let i = 0; i < slides.length; i++) {
+    //        slides[i].style.display = 'none';
+    //    }
+
+    dots.forEach((item) => item.classList.remove('dot-active'));
 
 
-       function hideTabContent(a) {
-    for (let i = a; i < tabContent.length; i++) 
-    {
-          tabContent[i].classList.remove('show');
-          tabContent[i].classList.add('hide');
-
-    }
-       }
-       hideTabContent(1);
-
-       function swowTabContent(b)
-       {
-           if(tabContent[b].classList.contains('hide')) {
-              tabContent[b].classList.remove('hide');
-              tabContent[b].classList.add('show');
-           }
-       }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+}
 
 
-       info.addEventListener('click', function(event)
-       {
-           let target = event.target;
-           if (target && target.classList.contains('info-header-tab')) {
-                for(let i = 0; i < tab.length; i++)
-                {
-                         if(target == tab[i])
-                         {
-                            hideTabContent(0);
-                            swowTabContent(i);
-                            break;
-                         }
-                }
-           }
-        
-       });
+   function plusSlides(n) {
+      showSlides(slideIndex += n);
+
+   }
+
+   function currentSlides(n) {
+       showSlides(slideIndex = n);
+   }
+   
+
+   prev.addEventListener('click', function() {
+       plusSlides(-1);
+   });
+  
+   next.addEventListener('click', function() {
+       plusSlides(1);
+   });
+
+   dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if(event.target.classList.contains('dot') && event.target == dots[i - 1]){
+                currentSlides(i);
+            }
+        }
+   });
+
 
 });
     
